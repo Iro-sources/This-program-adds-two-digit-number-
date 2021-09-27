4a1c10507fd77ff8dad8150f4033866ad4ffc55d
Express2021 <68305746+Mohamed-codes@users.noreply.github.com>
2021-09-27 18:24:44 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+first_number = two_digit_number[0]
+second_number = two_digit_number[1]
+print(first_number)
+print(second_number)
+print(int(first_number) + int(second_number))
+
+
+
+
